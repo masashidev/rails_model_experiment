2bf02a18df673171b6e7b17e7342838adb045f2c
Masashi.i <masashi.itamura.spt90@gmail.com>
2024-04-01 13:30:40 +0900

    add comments form and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,9 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    @post = Post.includes(:comments).find(params[:id])
+    @comment = Comment.new
+    @comments = @post.comments
   end
 
   # GET /posts/new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_01_021216) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_01_030231) do
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -34,5 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_01_021216) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
